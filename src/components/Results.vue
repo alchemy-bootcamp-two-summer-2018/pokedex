8802c78854b251cb3b6c22eda0add615e4375f44
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-21 15:25:20 -0700

    all pokemon displaying

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,30 +1,43 @@
 <template>
-  <div class="hello">
+  <div>
+    <ul>
+      <Tile v-for="tile in pokemon"
+      v-bind:key="tile.id"
+      v-bind:tile="tile"/>
+    </ul>
   </div>
    
 </template>
 
 <script>
+import pokemon from '../../pokedex.js';
+import Tile from './Tile.vue';
 export default {
   props: {
+  },
+  data(){
+    return {
+      pokemon
+    };
+  },
+  components: {
+    Tile
+  },
+  computed: {
+
+    // type() {
+    //   const typeOne = this.pokemon.map(type =>)
+    //   const typeTwo
+    // }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 5px;
 }
 </style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+      <!-- <div id="img" v-bind:style="{ backgroundImage: 'url(' + tile.url_image +')'}"> -->
+        <h3> {{ tile.pokemon }} </h3>
+        <!-- <div class="stats"> -->
+          <p>Type: {{ tile.type_1 }}</p>
+          <p>Attack: {{ tile.attack}}</p>
+          <p>Defense: {{ tile.defense}}</p>
+        <!-- </div> -->
+      <!-- </div> -->
+    </li>
+</template>
+
+<script>
+export default {
+  props: {
+    tile: Object
+  }  
+};
+</script>
+
+<style scoped>
+#img {
+  background-position:center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+li {
+  list-style: none;
+  text-align: center;
+  border: 1px solid #aaa;
+}
+</style>
