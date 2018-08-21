36a82f0f3a6933b5e633c66d21887493b135fb2c
Jack <ajackofsometrades@gmail.com>
2018-08-21 16:19:45 -0700

    filling out pokemon tile

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h2>{{ pokemon.pokemon }}</h2>
+    <h2>{{ pokemon.pokemon.toUpperCase() }}</h2>
+    <div class="stats">
+      <p>Type: {{ pokemon.type_1.toUpperCase() }}</p>
+      <p>Attack: {{ pokemon.attack }}</p>
+      <p>Defense: {{ pokemon.defense }}</p>
+    </div>
+    <img v-bind:src=pokemon.url_image />
   </div>
 </template>
 
@@ -12,5 +18,8 @@ export default {
 
 <style scoped>
 
+img {
+  width: 200px;
+}
 
 </style>
