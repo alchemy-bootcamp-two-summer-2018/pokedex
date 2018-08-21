a7e52036fd09bcbb57db6912c75a3bf6bd1738d0
Jack <ajackofsometrades@gmail.com>
2018-08-21 16:08:22 -0700

    sending list to results

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <Header/>
     <Menu/>
-    <Results/>
+    <Results v-bind:list="list"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 import Menu from './components/Menu.vue';
 import Results from './components/Results.vue';
 import pokedex from './services/pokedex.js';
@@ -21,7 +23,12 @@ export default {
     };
   },
   components: {
-    Header, Menu, Results
+    Header, Menu, Results, Footer
+  },
+  computed: {
+    list() {
+      return this.pokedex;
+    }
   }
 };
 </script>
