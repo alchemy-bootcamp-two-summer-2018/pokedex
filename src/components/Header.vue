ced2f6904b1f4d72d7b825602a6e11164ee67bcf
Jack <ajackofsometrades@gmail.com>
2018-08-21 15:44:34 -0700

    adding header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>Pokemon</h1>
+    <h2>Gotta catch 'em all!</h2>
+    <img alt="Pokemon logo" src="../assets/logo.png">
+  </header>
+</template>
+
+<script>
+
+
+export default {
+
+};
+</script>
+
+<style scoped>
+
+h1 {
+  font-size: 72px;
+  font-weight: 500;
+  padding-top: 15px;
+}
+img {
+  width: 300px;
+  margin: 25px;
+}
+
+</style>
