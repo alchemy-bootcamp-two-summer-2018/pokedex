64435427e64865e78a24e2e65aa1ea4ad0ed7459
Claire Flanagan <claireflanagan@gmail.com>
2018-08-21 16:39:49 -0700

    pokemon showing

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+        <h1>Pokedex</h1>
+        <Results
+          v-bind:pokemonList="pokemonData"/>
+  </div>
+</template>
+
+<script>
+import pokemonData from '../pokedex.js';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return { pokemonData };
+  },
+  components: {
+    Results
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h2>Results</h2>
+    <PokeTiles v-for="pokemon in pokemonList"
+    v-bind:key="pokemon.id"
+    v-bind:pokemonList="pokemon"/>
+</div>
+</template>
+
+<script>
+// import pokemonData from '../App.vue'
+import PokeTiles from './PokeTiles.vue'
+export default {
+    props: {
+        pokemonList: Array
+    },
+    components: {
+        PokeTiles
+    }
+}
+</script>
+<style>
+
+</style>
