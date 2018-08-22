7ee04fa569c77541b53355ee91ae72e715767a79
Jack <ajackofsometrades@gmail.com>
2018-08-21 17:59:28 -0700

    dynamic filtering, catching types and filter from menu

--- src/components/PokeFilter.vue
+++ src/components/PokeFilter.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="filters">
+
     <div id="type-filter">
       <p>Filter by type:</p>
-      <select v-model="selected">
-        <option value="all">All types</option>
-        <option>Fire</option>
-        <option>Grass</option>
-        <option>Water</option>
+      <select v-model="filter.type">
+        
+        <option value="all">
+          All types
+        </option>
+
+        <option 
+          v-for="type in types"
+          v-bind:key="type">
+            {{ type }}
+        </option>
+
       </select>
     </div>
+
     <div id="attack-filter">
       <p>Filter by attack:</p>
       <form>
         <input placeholder="Min value">
         <button>Filter</button>
       </form>
-
     </div>
-    
     
   </div>
 </template>
 
 <script>
 export default {
+  props: ['types', 'filter']
 
 };
 </script>
