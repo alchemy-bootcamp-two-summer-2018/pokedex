d9c930e8229b4b4171042a8d90858217dcb3631a
laurenshareshian <laurenshareshian@gmail.com>
2018-08-21 17:10:19 -0700

    updates correctly

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <Header />
-    <Results />
+    <header>
+      <PokemonFilter v-bind:onFilter="handleFilter"/>
+      <PokemonSort v-bind:onSort="handleSort"/>
+    </header>
+    <!-- <Header /> -->
+    <main>
+      <Pokemons v-bind:filter="filter"
+        v-bind:sort="sort"/>
+    </main> -->
   </div>
 </template>
 
 <script>
+import Pokemons from './components/Pokemons.vue';
+import PokemonFilter from './components/PokemonFilter.vue';
+import PokemonSort from './components/PokemonSort.vue';
 import Header from './components/Header.vue';
-import Results from './components/Results.vue';
 
 export default {
   name: 'app',
-  components: {
-    Header,
-    Results
+  data() {
+    return {
+      filter: {
+        attack: ''
+      },
+      sort: {
+        sort: 'pokemon',
+        direction: 1
+      }
+    };
   },
+  components: {
+    // Header,
+    Pokemons,
+    PokemonFilter,
+    PokemonSort,
+
+  },
+  methods: {
+    handleFilter(filter) {
+      this.filter = filter;
+    },
+    handleSort(sort) {
+      this.sort = sort;
+    }
+  }
 };
 </script>
 
