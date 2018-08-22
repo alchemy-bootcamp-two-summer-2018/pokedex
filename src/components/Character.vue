bca8a845303f17a9dc2623ee7c131ffa18bef2ae
Sophie Graham <sophiergraham1@gmail.com>
2018-08-22 11:05:38 -0700

    type showing fixed

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -2,7 +2,7 @@
   <li>
     <h3>{{ character.name }}</h3>
     <img class="images" v-bind:src="character.url_image">
-    <p>type: {{ character.type }}</p>
+    <p>type: {{ character.type_1 }}</p>
     <p>attack: {{ character.attack }}</p>
     <p>defense: {{ character.defense }}</p>
   </li>
