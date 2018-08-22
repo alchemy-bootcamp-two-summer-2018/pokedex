235ea56005aa20aeb68a54aeda9ecb445c0bdff2
Jack <ajackofsometrades@gmail.com>
2018-08-22 09:03:58 -0700

    unneeded form

--- src/components/Sort.vue
+++ src/components/Sort.vue
@@ -7,22 +7,6 @@
       <option value="attack">Attack</option>
       <option value="defense">Defense</option>
     </select>
-
-
-    <!-- <form v-on:submit.prevent="handleSubmit">
-      <select v-model="sort">
-        <option value="name">Name</option>
-        <option value="size">Size</option>
-        <option value="weight">Weight</option>
-      </select>
-      <label>
-        <input type="radio" value="1" name="direction" v-model="direction"> asc
-      </label>
-      <label>
-        <input type="radio" value="-1" name="direction" v-model="direction"> desc
-      </label>
-      <button>Apply Sort</button>
-    </form> -->
   </div>
 </template>
 
@@ -36,6 +20,11 @@ export default {
 
 p, select, div, form {
   display: inline;
+}
+
+#sort {
+  padding: 15px;
+  border: solid 1px rgb(138, 0, 11);
 }
 
 </style>
