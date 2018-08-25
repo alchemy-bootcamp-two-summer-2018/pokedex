ebf80aa04618d1f50d63f667bcfec4da97fa58ef
MikeBLambert <mlamb128@gmail.com>
2018-08-25 09:47:44 -0700

    Rebuilt Pokemon.vue from scratch

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,31 +1,27 @@
 <template>
     <li>
-        <!-- <img v-bind:src="pokemon.url_image"/> -->
-        <h3>Name: {{ pokemon.pokemon }} </h3>
-        <h3>Type 1: {{ pokemon.type_1 }} </h3>
-        <h3>Attack: {{ pokemon.attack }} </h3>
-        <h3>Defense: {{ pokemon.defense }} </h3>
+        <img v-bind:src="pokemon.url_image"/>
+        <h3>Name: {{ pokemon.pokemon }}</h3>
+        <h3>Type: {{ pokemon.type_1 }}</h3>
+        <h3>Attack: {{ pokemon.attack }}</h3>
+        <h3>Defense: {{ pokemon.defense }}</h3>
     </li>
 </template>
 
 <script>
-
 export default {
     props: {
-        pokemon: Object
+        pokemon: Object,
+        // selected: Object,
     }
 }
 </script>
 
 <style>
-
-
 li {
   list-style: none;
   text-align: center;
-  border: 1px solid #aaa;
-  background-color: rgb(192, 192, 245);
-  border: 5px solid black;
+  border: 5px solid #aaa;
 }
 
 img {
