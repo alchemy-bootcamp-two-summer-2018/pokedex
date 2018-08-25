5fb105041c4046b91663ec924ec7943f2101108f
MikeBLambert <mlamb128@gmail.com>
2018-08-25 09:47:56 -0700

    Rebuilt PokemonFilter from scratch

--- src/components/PokemonFilter.vue
+++ src/components/PokemonFilter.vue
@@ -1,52 +1,43 @@
 <template>
-    <form v-on:submit.prevent="handleSubmit">
-        <p>
-            <label>
-                Type 1:
-                <select v-model="selected">
-                    <option value="">All Types</option>
-                    <option v-for="type in types"
-                        v-bind:key="type"
-                        v-bind:value="type">
-                        {{ type }}
-                    </option>
-                </select>
-            </label>
-        </p>
-        <p>
-            <button>Apply</button>
-        </p>
-    </form>
+    <p>
+        <label>
+            <select v-model="selected" v-on:change="handleChange">
+                <option value="">All Pokemon</option>
+                <option v-for="type in types"
+                    v-bind:key="type"
+                    v-bind:value="type"
+                >
+                    {{ type }}
+                </option>
+            </select>
+        </label>
+    </p>
 </template>
 
 <script>
-// import pokedex from "../pokedex.js";
-import type from "./type.js";
-import Pokemon from './Pokemon.vue';
+import pokedex from '../pokedex.js';
+import data from './data.js'
 
 export default {
     props: {
         onFilter: Function
     },
-    data(){
+    data() {
         return {
-            types: type.getTypes(),
-            selected: ''
-        };
+            types: data.getTypes(),
+            selected:'',
+        }
     },
     methods: {
-        handleSubmit() {
+        handleChange() {
             const filter = {
                 type: this.selected
             };
             this.onFilter(filter);
         }
-    },
-    components: {
-        Pokemon
     }
-};
-
+    
+}
 </script>
 
 <style>
